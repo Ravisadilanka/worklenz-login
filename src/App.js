cb57df123ff76207793259d9fbc0011b939f1856
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-09-09 15:22:11 +0530

    Designed Reset password page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Reset_Password from './pages/Reset_Password';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/reset-password" element={<Reset_Password />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Reset_Password.js
+++ src/pages/Reset_Password.js
@@ -7,7 +7,7 @@ import googleIcon from '../assets/google.png'
 
 const { Text, Link, Title } = Typography;
 
-const Login = () => {
+const Reset_Password = () => {
     const onFinish = (values) => {
         console.log('Received values of form: ', values);
     };
@@ -36,7 +36,10 @@ const Login = () => {
                     }}
                 >
                     <img src={Logo} alt="Logo" style={{ height: 50 }} />
-                    <Text type="secondary">Log Into your account.</Text>
+                    <Flex justify="center" align="center" style={{textAlign: "center"}}>
+
+                    <Text type="secondary">Please enter your email address. You will receive a link to create a new password via email.</Text>
+                    </Flex>
                     <Form
                         labelCol={{ span: 18 }}
                         wrapperCol={{ span: 36 }}
@@ -49,37 +52,19 @@ const Login = () => {
                             name="email"
                             rules={[{ required: true, message: 'Please input your Email!' }]}
                         >
-                            <Input prefix={<UserOutlined />} placeholder="Email" style={{ height: '40px', fontSize: '16px' }} />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[{ required: true, message: 'Please input your Password!' }]}
-                        >
-                            <Input prefix={<LockOutlined />} type="password" placeholder="Password" suffix={<EyeOutlined />} style={{ height: '40px', fontSize: '16px' }} />
-                        </Form.Item>
-                        <Form.Item>
-                            <Flex justify="space-between" align="center">
-                                <Form.Item name="remember" valuePropName="checked" noStyle>
-                                    <Checkbox>Remember me</Checkbox>
-                                </Form.Item>
-                                <a href="/reset-password">Forgot password</a>
-                            </Flex>
+                            <Input prefix={<UserOutlined />} placeholder="Enter your email" style={{ height: '40px', fontSize: '16px' }} />
                         </Form.Item>
 
                         <Form.Item>
                             <Flex vertical gap={4}>
                                 <Button block type="primary" htmlType="submit" size="large" style={{ borderRadius: "4px" }}>
-                                    <Text style={{ fontSize: '16px', color: "white" }}>Log in</Text>
+                                    <Text style={{ fontSize: '16px', color: "white" }}>Reset Password</Text>
                                 </Button>
                                 <Flex justify="center">OR</Flex>
                                 <Button block size="large" style={{ borderRadius: "4px" }}>
-                                    <img style={{ width: '18px' }} src={googleIcon} alt="google icon" />
-                                    <Text style={{ fontSize: '16px' }}>Sign in with Google</Text>
+                                    <Text style={{ fontSize: '16px' }}>Return to Login</Text>
                                 </Button>
                             </Flex>
-                        </Form.Item>
-                        <Form.Item>
-                        <Text>Don't have an account? <a href="/signup">Sign Up</a></Text>
                         </Form.Item>
                     </Form>
                 </Flex>
@@ -88,4 +73,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Reset_Password;
